b593f30ab22c6cffba0a3c9403baf63ad44f5444
utkarshvs14 <102665950+utkarshvs14@users.noreply.github.com>
2022-05-20 19:38:54 +0530

    Add files via upload

---
+++ EurekaServerService/src/test/java/saket/EurekaServerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package saket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
